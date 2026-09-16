7e16c56569a80d26cd6cf54a4567ca352839ef96
E.Wolf <e.wolf@yudole.ru>
2026-09-16 18:57:45 +0300

    dev: keep thumbnail left-aligned in mobile card

--- app/components/ImagesTable.vue
+++ app/components/ImagesTable.vue
@@ -517,13 +517,13 @@ async function onAltBlur(event: Event, img: ImageResponse) {
 
   .images-table td.col-thumb {
     grid-area: thumb;
-    text-align: center;
+    text-align: left;
   }
 
   .images-table td.col-thumb img {
     width: 120px;
     height: 90px;
-    margin: 0 auto;
+    margin-right: auto;
   }
 
   .images-table td.col-alt {
